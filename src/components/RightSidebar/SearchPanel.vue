0c803741e036a60cd85922eb7aa753dcca4f9327
ys928 <it_yushi@163.com>
2024-06-15 07:17:39 +0800

    1.完成搜索面板UI替换

--- src/components/RightSidebar/SearchPanel.vue
+++ src/components/RightSidebar/SearchPanel.vue
@@ -2,17 +2,18 @@
 import { Ref, ref, onMounted } from 'vue';
 import { dialog } from '@tauri-apps/api';
 import { fs } from '@tauri-apps/api';
-import { NIcon, NInput, NSpin, NScrollbar } from "naive-ui"
-import { FolderMinus } from "@vicons/tabler"
 import { useNovelStore } from '../../store/novel';
 import { FS, NovelInfo } from '../../api/fs';
-
+import { ElScrollbar, ElInput, ElIcon } from 'element-plus';
+import { Folder } from '@element-plus/icons-vue'
 const novel_store = useNovelStore();
 
 //控制要显示的小说列表项
 let show_novel_list = ref([]) as Ref<Array<NovelInfo>>;
 //控制加载图标是否显示
 const show_loading = ref(false);
+
+const search_input = ref('');
 
 //存储所有可搜索的小说项
 let all_novel: Array<NovelInfo> = [];
@@ -86,15 +87,18 @@ function dclick_novel(index: number) {
     <div class="SearchPanel">
         <div class="title">搜索栏</div>
         <div class="top_pos">
-            <n-input size="tiny" round @input="search_fun" placeholder="搜小说"></n-input>
-            <n-icon class="icon" size="20" color="#787878" :component="FolderMinus" @click="choose_dir"></n-icon>
+            <el-input v-model="search_input" @input="search_fun" placeholder="搜小说" size="small"></el-input>
+            <el-icon class="icon" @click="choose_dir" color="#787878" size="20" title="选择目录">
+                <Folder />
+            </el-icon>
         </div>
-        <n-scrollbar class="novel_list">
-            <div v-for="(item, index) in show_novel_list" class="novel_item" @dblclick="dclick_novel(index)">
-                {{ item.name!.substring(0, item.name!.lastIndexOf('.')) }}
-            </div>
-            <n-spin class="loading" size="medium" v-show="show_loading"></n-spin>
-        </n-scrollbar>
+        <div class="novel_list" v-loading="show_loading">
+            <el-scrollbar>
+                <div v-for="(item, index) in show_novel_list" class="novel_item" @dblclick="dclick_novel(index)">
+                    {{ item.name!.substring(0, item.name!.lastIndexOf('.')) }}
+                </div>
+            </el-scrollbar>
+        </div>
     </div>
 </template>
 
@@ -157,14 +161,6 @@ function dclick_novel(index: number) {
                 border: #36ad6a solid 1px;
                 color: #36ad6a;
             }
-        }
-
-        .loading {
-            width: 50px;
-            height: 50px;
-            position: absolute;
-            left: 50%;
-            transform: translate(-50%, 0);
         }
     }
 }
